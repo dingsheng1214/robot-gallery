1c98d2cc4a8b1e5875eb44ba7ad1fd1b6153a9f8
dingsheng <dingduoduo0916@gmail.com>
2022-04-11 23:28:04 +0800

    feat: 媒体文件与字体的使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './assets/images/logo.svg'
 import styles from './App.module.css';
 import robots from './mock/robot.json'
 import Robot from './components/Robot'
@@ -6,6 +7,10 @@ import Robot from './components/Robot'
 function App() {
   return (
       <div className={styles.app}>
+          <div className={styles.appHeader}>
+              <img src={logo} className={styles.appLogo} alt=""/>
+          </div>
+          <h1>罗伯特机器人狂拽炫酷吊炸天online在线购物平台名字要长</h1>
           <div className={styles.robotList}>
               {
                   robots.map(({id, name, email}) => <Robot key={id} id={id} name={name} email={email} />)
