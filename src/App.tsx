11946d65a23218f9cf08fcbcaa9ce882a4c0fdf2
dingsheng <dingduoduo0916@gmail.com>
2022-04-12 15:29:48 +0800

    生命周期中发送异步请求

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,49 @@ import robots from './mock/robot.json'
 import Robot from './components/Robot'
 import ShoppingCart from './components/ShoppingCart'
 
-function App() {
-  return (
-      <div className={styles.app}>
-          <div className={styles.appHeader}>
-              <img src={logo} className={styles.appLogo} alt=""/>
-          </div>
-          <h1>罗伯特机器人狂拽炫酷吊炸天online在线购物平台名字要长</h1>
+interface IRobot {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+interface Props {}
+interface State {
+  robots: IRobot[];
+}
 
-          <ShoppingCart />
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      robots: robots,
+    }
+  }
+  async componentDidMount() {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users')
+      const json = await res.json()
+      this.setState({
+        robots: json
+      })
+  }
+  render() {
+      return (
+          <div className={styles.app}>
+              <div className={styles.appHeader}>
+                  <img src={logo} className={styles.appLogo} alt=""/>
+              </div>
+              <h1>罗伯特机器人狂拽炫酷吊炸天online在线购物平台名字要长</h1>
 
-          <div className={styles.robotList}>
-              {
-                  robots.map(({id, name, email}) => <Robot key={id} id={id} name={name} email={email} />)
-              }
+              <ShoppingCart />
+
+              <div className={styles.robotList}>
+                  {
+                      this.state.robots.map(({id, name, email}) => <Robot key={id} id={id} name={name} email={email} />)
+                  }
+              </div>
           </div>
-      </div>
-  );
+      );
+  }
 }
 
 export default App;
